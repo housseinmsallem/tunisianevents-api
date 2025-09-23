f7cba3182d55535483dbfb37e096a2701970b7ff
Houssein Msallem <houssein.msallem@gmail.com>
2025-09-23 13:50:59 +0100

    media resource

--- src/media/media.controller.ts
+++ src/media/media.controller.ts
@@ -1,26 +1,31 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Post, UploadedFile, Query } from '@nestjs/common';
 import { MediaService } from './media.service';
-import { CreateMediaDto } from './dto/create-media.dto';
-import { UpdateMediaDto } from './dto/update-media.dto';
+import { UploadFileRequestDTO } from './dto/upload-media.dto';
+import { ApiBody, ApiConsumes } from '@nestjs/swagger';
+import { Organisation } from 'src/organisation/entities/organisation.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Controller('media')
 export class MediaController {
   constructor(private readonly mediaService: MediaService) {}
-
-  @Post({
-    path: '/upload',
-    description: 'upload media',
-    response: UploadFinalizeMediaResponseDTO,
+  @Post('upload')
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
   })
-  uploadMedia(@Body() createMediaDto: CreateMediaDto) {
-    return this.mediaService.create(createMediaDto);
+  uploadMedia(
+    @UploadedFile() file: Express.Multer.File,
+    @Query() data: UploadFileRequestDTO,
+    @CurrentUser() user: Organisation | User,
+  ) {
+    return;
   }
 }
--- src/media/media.service.ts
+++ src/media/media.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { CreateMediaDto } from './dto/create-media.dto';
+import { CreateMediaDto } from './dto/upload-media.dto';
 import { UpdateMediaDto } from './dto/update-media.dto';
 
 @Injectable()
